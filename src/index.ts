d52bea1d3a7f23ea9166ca489b8514e2159fe8cc
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-07 18:31:29 +0100

    chore: export theme and utils

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import * as theme from "./core/theme"
+import * as utils from "./core/utils"
+
+export { theme, utils }
+
 export { Button } from "./components/button/button"
-export { ButtonProps } from "./components/button/button.types"
+export { ButtonProps } from "./components/button/button.props"
+
+export { Flex } from "./components/flex/flex"
+export { FlexProps } from "./components/flex/flex.props"
