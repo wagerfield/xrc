98c48f58697aad1692c31c42051d8dabee9b4457
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-23 12:52:17 +0100

    feat: implement prop and style utils

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,11 +1,22 @@
-import { Pred as Predicate } from "ramda"
+import { prop as rProp, Pred as Predicate } from "ramda"
 import { Interpolation } from "styled-components"
 import { Theme, ThemeProps } from "../themes/theme.types"
 
-export const styleWhen = <P>(predicate: Predicate, style: Interpolation<P>) => {
-  return (props: P) => predicate(props) && style
-}
+export const styleIfElse = <P>(
+  predicate: Predicate,
+  styleWhenTrue?: Interpolation<P>,
+  styleWhenFalse?: Interpolation<P>
+) => (props: P) => (predicate(props) ? styleWhenTrue : styleWhenFalse)
+
+export const styleWhen = <P>(predicate: Predicate, style: Interpolation<P>) =>
+  styleIfElse(predicate, style, null)
+
+export const styleUnless = <P>(predicate: Predicate, style: Interpolation<P>) =>
+  styleIfElse(predicate, null, style)
 
 export const theme = <K extends keyof Theme, P extends ThemeProps>(key: K) => {
   return (props: P) => props.theme && props.theme[key]
 }
+
+export const prop = <P>(key: Extract<keyof P, string>) =>
+  rProp(key) as Interpolation<P>
