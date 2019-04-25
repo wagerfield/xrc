cec54b1c4310082ce0487efe3b5348995d640ba8
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-25 18:30:26 +0100

    chore: update master theme

--- src/themes/master.theme.ts
+++ src/themes/master.theme.ts
@@ -1,66 +1,7 @@
-import Theme from "./theme.types"
+import { Theme } from "../types/theme.types"
 
 export const MasterTheme: Theme = {
-  // Font Family
-  fontFamilyHeading: "Avenir Next",
-  fontFamilyRegular: "Avenir Next",
-
-  // Font Size
-  fontSizeHeading1: "32px",
-  fontSizeHeading2: "28px",
-  fontSizeHeading3: "24px",
-  fontSizeRegular: "16px",
-  fontSizeSmall: "14px",
-
-  // Font Weight
-  fontWeightRegular: 400,
-  fontWeightBold: 700,
-
-  // Line Height
-  lineHeightHeading: 1.25,
-  lineHeightRegular: 1.5,
-  lineHeightReset: 1.0,
-
-  // Color: Brand
-  colorBrandPrimary: "#00B67D",
-  colorBrandSecondary: "#FF6A00",
-
-  // Color: Background
-  colorBackgroundPrimary: "#F7F4EF",
-  colorBackgroundSecondary: "#DDDAD4",
-  colorBackgroundContrast: "#FFFFFF",
-  colorBackgroundDebug: "rgba(0,153,255,0.25)",
-
-  // Color: Text
-  colorTextPrimary: "#202020",
-  colorTextSecondary: "#666666",
-  colorTextContrast: "#FFFFFF",
-
-  // Color: Feedback
-  colorFeedbackNeutral: "#0277BD",
-  colorFeedbackNegative: "#D50000",
-  colorFeedbackPositive: "#00B67D",
-
-  // Color: Overlay
-  colorOverlayLight: "rgba(0,0,0,0.04)",
-  colorOverlayMedium: "rgba(0,0,0,0.3)",
-  colorOverlayDark: "rgba(0,0,0,0.6)",
-
-  // Border
-  border: "2px solid",
-
-  // Size
-  sizeLarge: "48px",
-  sizeMedium: "40px",
-  sizeSmall: "32px",
-
-  // Radius
-  radiusLarge: "24px",
-  radiusMedium: "20px",
-  radiusSmall: "16px",
-
-  // Shadow
-  shadowButton: "0 2px 4px rgba(0,0,0,0.16), 0 -4px 0 rgba(0,0,0,0.32) inset"
+  space: [0, 4, 8, 16, 32, 64, 128, 256]
 }
 
 export default MasterTheme
