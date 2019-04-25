508fce63316ea879bec80e39c8cf36dec258f416
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-25 18:24:39 +0100

    refactor: remove files

--- src/types/styled-components.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components"
-import Theme from "../themes/theme.types"
-
-declare module "styled-components" {
-  // tslint:disable-next-line
-  export interface DefaultTheme extends Theme {}
-}
