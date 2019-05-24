9d0f4f393f0e84bf620d203e61956aa08592031e
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-24 22:43:47 +0100

    feat: add render functions

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,10 +1,22 @@
 import { FunctionComponent } from "react"
-import { jsx, css } from "@emotion/core"
+import { css, jsx } from "@emotion/core"
 import {
   compose,
+  backgroundSet,
+  BackgroundSetProps,
+  BackgroundSetStyle,
+  borderSet,
+  BorderSetProps,
+  BorderSetStyle,
+  boxShadow,
+  BoxShadowProps,
+  BoxShadowStyle,
   colorSet,
   ColorSetProps,
   ColorSetStyle,
+  displaySet,
+  DisplaySetProps,
+  DisplaySetStyle,
   layoutSet,
   LayoutSetProps,
   LayoutSetStyle,
@@ -14,22 +26,38 @@ import {
   propTypes
 } from "onno-react"
 
-export type BoxProps = ColorSetProps & LayoutSetProps & SpaceSetProps
+export type BoxProps = BackgroundSetProps &
+  BorderSetProps &
+  BoxShadowProps &
+  ColorSetProps &
+  DisplaySetProps &
+  LayoutSetProps &
+  SpaceSetProps
 
-export type BoxStyle = ColorSetStyle & LayoutSetStyle & SpaceSetStyle
+export type BoxStyle = BackgroundSetStyle &
+  BorderSetStyle &
+  BoxShadowStyle &
+  ColorSetStyle &
+  DisplaySetStyle &
+  LayoutSetStyle &
+  SpaceSetStyle
 
-export const boxStyles = css({
+const boxStyles = css({
   boxSizing: "border-box"
 })
 
-export const boxSet = compose<BoxProps, BoxStyle>(
+const boxSet = compose<BoxProps, BoxStyle>(
+  backgroundSet,
+  borderSet,
+  boxShadow,
   colorSet,
+  displaySet,
   layoutSet,
   spaceSet
 )
 
 export const Box: FunctionComponent<BoxProps> = (props) => (
-  <div css={[boxStyles, boxSet(props)]} />
+  <div css={[boxStyles, boxSet(props)]} children={props.children} />
 )
 
 Box.propTypes = propTypes(boxSet)
