93b561c81752758d08ca664e03106a0ebd2f8170
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-25 08:50:45 +0100

    chore: update code example

--- repl/src/app.jsx
+++ repl/src/app.jsx
@@ -1,12 +1,36 @@
 import React from "react"
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
+import * as scope from "xrc"
 
-const code = `<h1>Hello World</h1>`
+const code = `
+<Base>
+  <Box
+    p="6"
+    m="5"
+    d="flex"
+    jfc="center"
+    bg="fill.main"
+  >
+    <Box
+      px="5"
+      py="4"
+      h="lg"
+      rad="lg"
+      d="inline-block"
+      bsh="drop.1"
+      cst="primary"
+      tst="caps"
+      ff="main"
+      lh="0"
+    >Hello World</Box>
+  </Box>
+</Base>
+`
 
 export default () => (
-  <LiveProvider code={code}>
+  <LiveProvider scope={scope} code={code.trim()}>
+    <LivePreview />
     <LiveEditor />
     <LiveError />
-    <LivePreview />
   </LiveProvider>
 )
