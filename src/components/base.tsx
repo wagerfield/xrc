15e7b44cb9430a50564840ab2f262631b7958dfa
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-25 22:26:25 +0100

    refactor: add globalStyle variant

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,16 +1,28 @@
-import { jsx } from "@emotion/core"
-import { ElementType, FunctionComponent } from "react"
+import { FunctionComponent } from "react"
+import { jsx, Global } from "@emotion/core"
+import { colorSet, spaceSet, textSet, variant } from "onno-react"
+import { MasterTheme } from "../themes/master"
 import { Theme } from "../themes/types"
 
 export interface BaseProps {
-  as: ElementType
-  theme?: Theme
+  theme: Theme
 }
 
+const globalStyle = variant({
+  propsKeys: ["global"],
+  themeKeys: ["globalStyles"],
+  renderers: [colorSet, spaceSet, textSet]
+})
+
 export const Base: FunctionComponent<BaseProps> = (props) => (
-  <props.as css={[]} children={props.children} />
+  <Global
+    styles={globalStyle({
+      global: [],
+      theme: props.theme
+    })}
+  />
 )
 
 Base.defaultProps = {
-  as: "div"
+  theme: MasterTheme
 }
