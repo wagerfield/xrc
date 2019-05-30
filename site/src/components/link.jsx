f41ad3cb19f3d47d313513899b0a785c759192f5
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-31 00:06:01 +0100

    chore: remove comment

--- site/src/components/link.jsx
+++ site/src/components/link.jsx
@@ -3,6 +3,5 @@ import { Link as RouterLink } from "@reach/router"
 import { mapPath } from "../core/utils"
 
 export const Link = ({ children, href }) => {
-  console.log(href)
   return <RouterLink to={mapPath(href)} children={children} />
 }
