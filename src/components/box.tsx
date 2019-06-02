edaf55800c4fdbe04ec62dc27354cd9d7b5d7ef6
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-02 16:47:20 +0100

    refactor: move renderers to separate file

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,67 +1,7 @@
-import { ElementType, FunctionComponent } from "react"
-import { withTheme } from "emotion-theming"
 import { css, jsx } from "@emotion/core"
-import {
-  compose,
-  backgroundSet,
-  BackgroundSetProps,
-  BackgroundSetStyle,
-  borderSet,
-  BorderSetProps,
-  BorderSetStyle,
-  boxShadow,
-  BoxShadowProps,
-  BoxShadowStyle,
-  colorSet,
-  ColorSetProps,
-  ColorSetStyle,
-  colorStyle,
-  ColorStyleProps,
-  displaySet,
-  DisplaySetProps,
-  DisplaySetStyle,
-  flexSet,
-  FlexSetProps,
-  FlexSetStyle,
-  gridSet,
-  GridSetProps,
-  GridSetStyle,
-  layoutSet,
-  LayoutSetProps,
-  LayoutSetStyle,
-  spaceSet,
-  SpaceSetProps,
-  SpaceSetStyle,
-  textSet,
-  TextSetProps,
-  TextSetStyle,
-  textStyle,
-  TextStyleProps
-} from "onno-react"
-
-type BoxSetProps = BackgroundSetProps &
-  BorderSetProps &
-  BoxShadowProps &
-  ColorSetProps &
-  ColorStyleProps &
-  DisplaySetProps &
-  FlexSetProps &
-  GridSetProps &
-  LayoutSetProps &
-  SpaceSetProps &
-  TextSetProps &
-  TextStyleProps
-
-type BoxSetStyle = BackgroundSetStyle &
-  BorderSetStyle &
-  BoxShadowStyle &
-  ColorSetStyle &
-  DisplaySetStyle &
-  FlexSetStyle &
-  GridSetStyle &
-  LayoutSetStyle &
-  SpaceSetStyle &
-  TextSetStyle
+import { withTheme } from "emotion-theming"
+import { ElementType, FunctionComponent } from "react"
+import { boxSet, BoxSetProps } from "../core/renderers"
 
 export interface BoxProps extends BoxSetProps {
   as?: ElementType
@@ -70,21 +10,6 @@ export interface BoxProps extends BoxSetProps {
 const boxStyles = css({
   boxSizing: "border-box"
 })
-
-const boxSet = compose<BoxSetProps, BoxSetStyle>(
-  backgroundSet,
-  borderSet,
-  boxShadow,
-  colorSet,
-  colorStyle,
-  displaySet,
-  flexSet,
-  gridSet,
-  layoutSet,
-  spaceSet,
-  textSet,
-  textStyle
-)
 
 export const Box: FunctionComponent<BoxProps> = withTheme((props) => (
   <props.as css={[boxStyles, boxSet(props)]} children={props.children} />
