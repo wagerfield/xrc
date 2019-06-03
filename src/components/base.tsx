52fd2aaba961abfcaab678822234d29c8c5fcb1e
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-03 15:05:42 +0100

    refactor: normalizeStyle > normalizeStyles

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -3,7 +3,7 @@ import { isArray, globalStyle } from "onno-react"
 import { jsx, InterpolationWithTheme } from "@emotion/core"
 import { Theme } from "../themes/types"
 import { MasterTheme } from "../themes/master"
-import { normalize as normalizeStyle } from "../core/normalize"
+import { normalize as normalizeStyles } from "../core/normalize"
 import { renderFonts, FontFaceOptions } from "../core/fonts"
 import { Provider, ProviderProps } from "./provider"
 import { Global } from "./global"
@@ -21,7 +21,7 @@ export const Base: FunctionComponent<BaseProps> = ({
 }) => {
   const styles: InterpolationWithTheme<Theme> = globalStyle({ theme }) || []
   if (isArray(fonts)) styles.unshift.apply(styles, renderFonts(fonts) as any)
-  if (normalize) styles.unshift(normalizeStyle as any)
+  if (normalize) styles.unshift(normalizeStyles as any)
   return (
     <Provider theme={theme}>
       <Global styles={styles} />
