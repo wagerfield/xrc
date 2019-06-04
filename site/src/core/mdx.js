573d778e367eba634595de98a951cb9178ba6c65
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-04 22:56:15 +0100

    feat: add xrc Box component to MDX component scope

--- site/src/core/mdx.js
+++ site/src/core/mdx.js
@@ -1,4 +1,11 @@
+import { Box } from "xrc"
 import { Code as code } from "components/code"
 import { Link as a } from "components/link"
 
-export const components = { a, code }
+export const components = {
+  // Elements
+  a,
+  code,
+  // Components
+  Box
+}
