fb05af9fb690cfdbf00da4770a0487d36c193d73
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-05 22:17:15 +0100

    feat: add css prop

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,24 +1,34 @@
-import { omit } from "onno-react"
-import { css, jsx } from "@emotion/core"
+import { interpolate, omit } from "onno-react"
 import { withTheme } from "emotion-theming"
+import { css, jsx, InterpolationWithTheme } from "@emotion/core"
 import { ElementType, FunctionComponent } from "react"
 import { boxSet, BoxSetProps } from "../core/renderers"
+import { Theme } from "../themes/types"
 
 export interface BoxProps extends BoxSetProps {
+  css?: InterpolationWithTheme<Theme>
   as?: ElementType
 }
 
-const boxStyles = css({
+const styles = css({
   boxSizing: "border-box"
 })
 
 const sanitize = omit({
-  propsKeys: ["as"],
+  propsKeys: ["as", "css"],
+  renderers: [boxSet]
+})
+
+const render = interpolate({
+  name: "box",
   renderers: [boxSet]
 })
 
 export const Box: FunctionComponent<BoxProps> = withTheme((props) => (
-  <props.as css={[boxStyles, boxSet(props)]} {...sanitize(props)} />
+  <props.as
+    css={[styles, boxSet(props), render(props.css, props.theme)]}
+    {...sanitize(props)}
+  />
 ))
 
 Box.defaultProps = {
