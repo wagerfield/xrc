d7c2e50493b45680c64e78fc0dd6202baac03d75
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-05 10:22:59 +0100

    style: replace Base with Box

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import { REPL } from "components/repl"
 
 export const code = `
-<Base>
+<Box background="fill.main">
   <h1>👋 Hejsan</h1>
   <p>Welcome to the <code>xrc</code> docs. Here you will find everything you need to get up and running.</p>
   <button>Get started</button>
-</Base>
+</Box>
 `
 
 export default () => <REPL code={code} />
