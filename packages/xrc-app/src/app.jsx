d5645234ade6df319bc1a1383f23fdccd7ebb720
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-06 22:35:34 +0100

    chore: remove MDX provider

--- packages/xrc-app/src/app.jsx
+++ packages/xrc-app/src/app.jsx
@@ -2,22 +2,20 @@ import React from "react"
 import { Base } from "xrc"
 import { Router } from "@reach/router"
 import { Routes, Root } from "react-static"
-import { MDXProvider } from "@mdx-js/react"
+// import { MDXProvider } from "@mdx-js/react"
 import { Loading } from "./components/loading"
 import { Nav } from "./components/nav"
 import { fonts } from "./core/fonts"
-import { components } from "./core/mdx"
+// import { components } from "./core/mdx"
 
 export default () => (
   <Root>
     <Base fonts={fonts}>
       <Nav />
       <React.Suspense fallback={<Loading />}>
-        <MDXProvider components={components}>
-          <Router component="main">
-            <Routes default />
-          </Router>
-        </MDXProvider>
+        <Router>
+          <Routes default />
+        </Router>
       </React.Suspense>
     </Base>
   </Root>
