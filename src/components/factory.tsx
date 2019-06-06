ccc5eeed55b26bb63296d6576f76b0a48f86fc79
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-06 12:25:32 +0100

    feat: implement prop forwarding

--- src/components/factory.tsx
+++ src/components/factory.tsx
@@ -1,13 +1,14 @@
 import { ElementType, FunctionComponent } from "react"
+import { jsx, InterpolationWithTheme, ObjectInterpolation } from "@emotion/core"
+import { withTheme } from "emotion-theming"
 import {
   omit,
-  interpolate,
+  pick,
   isType,
+  interpolate,
   ThemeProps,
   AnyRenderFunction
 } from "onno-react"
-import { jsx, InterpolationWithTheme } from "@emotion/core"
-import { withTheme } from "emotion-theming"
 import { Theme } from "../themes/types"
 
 export type Interpolation = InterpolationWithTheme<Theme>
@@ -15,36 +16,59 @@ export type Interpolation = InterpolationWithTheme<Theme>
 export type InterpolationFunction<P> = (props: P) => Interpolation
 
 export interface ComponentProps extends ThemeProps {
-  css?: Interpolation
   as?: ElementType
+  css?: ObjectInterpolation<any>
+  style?: ObjectInterpolation<any>
+  forward?: string[]
+}
+
+export interface DefaultProps {
+  as: ElementType
 }
 
 export interface ComponentOptions<P extends ComponentProps> {
   name: string
   renderers: AnyRenderFunction[]
+  defaultProps: DefaultProps & Partial<P>
   styles?: Interpolation | InterpolationFunction<P>
-  defaultProps?: Partial<P>
 }
 
-const propsKeys = ["as", "css"]
-
-const isFunction = isType<InterpolationFunction<any>>("function")
+const propsKeys = ["as", "css", "style", "forward"]
 
 export function createComponent<P extends ComponentProps>({
   name,
   styles,
   renderers,
-  defaultProps = { as: "div" } as P
+  defaultProps
 }: ComponentOptions<P>): FunctionComponent<P> {
-  const sanitize = omit({ propsKeys, renderers })
-  const transform = interpolate({ name, renderers })
+  const sanitize = omit<P>({ propsKeys, renderers })
+  const transform = interpolate<P, any>({ name, renderers })
+  const isFunction = isType<InterpolationFunction<P>>("function")
+
+  // Create themed component
   const component: FunctionComponent<P> = withTheme((props) => {
     const baseStyles = isFunction(styles) ? styles(props) : styles
     const userStyles = transform(props.css, props.theme)
     const propStyles = transform.renderer(props)
-    const styleArray = [baseStyles, propStyles, userStyles]
-    return <props.as css={styleArray} {...sanitize(props)} />
+
+    // Prepare component styles
+    const injectedStyles = [baseStyles, propStyles, userStyles]
+    const inlineStyles = transform(props.style, props.theme)
+
+    // Prepare component props
+    const forward = pick<P>({ propsKeys: props.forward })
+    const componentProps = {
+      ...sanitize(props),
+      ...forward(props),
+      css: injectedStyles,
+      style: inlineStyles
+    }
+
+    // Apply styles and sanitized props
+    return <props.as {...componentProps} />
   })
+
+  // Define component properties and return
   Object.defineProperty(component, "name", { value: name })
   component.defaultProps = defaultProps
   return component
