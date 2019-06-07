96c6d7b9ea70819fe3fd9c7fbd5347951c655f3d
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-07 22:51:20 +0100

    revert: mdx provider

--- packages/xrc-app/src/app.jsx
+++ packages/xrc-app/src/app.jsx
@@ -2,23 +2,23 @@ import React from "react"
 import { Base } from "xrc"
 import { Router } from "@reach/router"
 import { Routes, Root } from "react-static"
-// import { MDXProvider } from "@mdx-js/react"
+import { MDXProvider } from "@mdx-js/react"
 import { Loading } from "./components/loading"
 import { Nav } from "./components/nav"
 import { fonts } from "./core/fonts"
-// import { components } from "./core/mdx"
-
-// <MDXProvider components={components}>
+import { components } from "./core/mdx"
 
 export default () => (
   <Root>
     <Base fonts={fonts}>
-      <Nav />
-      <React.Suspense fallback={<Loading />}>
-        <Router component="main">
-          <Routes default />
-        </Router>
-      </React.Suspense>
+      <MDXProvider components={components}>
+        <Nav />
+        <React.Suspense fallback={<Loading />}>
+          <Router component="main">
+            <Routes default />
+          </Router>
+        </React.Suspense>
+      </MDXProvider>
     </Base>
   </Root>
 )
