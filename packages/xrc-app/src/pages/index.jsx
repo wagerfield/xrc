152f8758532ad13cb3b4a1f883e0042ceb78e6a6
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-08 16:38:28 +0100

    chore: update example

--- packages/xrc-app/src/pages/index.jsx
+++ packages/xrc-app/src/pages/index.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import { REPL } from "../components/repl"
 
 export const code = `
-<Box variant="main">
-  <Heading>👋 Hejsan</Heading>
-  <Text>Welcome to the <code>xrc</code> docs. Here you will find everything you need to get up and running.</Text>
-  <button>Get started</button>
-  <Image display="block" src="http://www.fillmurray.com/200/200" />
+<Box bg="overlay.dark" p="5">
+  <Box var="main" p="5" rad="lg" bsh="lg">
+    <Heading>👋 Hejsan</Heading>
+    <Text>Welcome to the <code>xrc</code> docs. Here you will find everything you need to get up and running.</Text>
+    <Button var="secondary" my="5">Get started</Button>
+    <Image display="block" src="http://www.fillmurray.com/200/200" />
+  </Box>
 </Box>
 `
 
