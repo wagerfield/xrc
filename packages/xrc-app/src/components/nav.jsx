e6bacd026188b2a0dc30bba3c0850dcae6bb9eee
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-10 18:25:05 +0100

    feat: implement nav components

--- packages/xrc-app/src/components/nav.jsx
+++ packages/xrc-app/src/components/nav.jsx
@@ -1,38 +1,56 @@
 import React from "react"
-import { Box, Text } from "xrc"
 import { Link } from "@reach/router"
+import { omit, isArray } from "onno"
+import { Logo, Text, Box } from "xrc"
 
-export const NavLink = ({ to, children, justifySelf }) => (
-  <Text
-    as={Link}
-    to={to}
-    children={children}
-    paddingX={12}
-    paddingY={3}
-    borderRadius="4px"
-    lineHeight="single"
-    fontSmoothing="antialiased"
-    css={{
-      ":active,:hover": {
-        background: "overlay.light"
-      }
-    }}
+const hoverStyles = {
+  ":active,:hover": {
+    background: "overlay.light"
+  }
+}
+
+const omitNavProps = omit({
+  propsKeys: ["links"]
+})
+
+const mapLink = (props, index) => (
+  <NavLink
+    key={props.key}
+    to={props.path}
+    ml={!index ? "auto" : null}
+    children={props.text}
   />
 )
 
-export const Nav = () => (
+export const Nav = (props) => (
   <Box
     as="nav"
-    position="relative"
-    zIndex="1"
     display="flex"
-    padding="4"
     width="1"
-    textStyle="caps"
+    height="xl"
+    paddingX="3"
+    paddingY="3"
+    alignItems="center"
     background="fill.main"
+    zIndex="1"
+    {...omitNavProps(props)}
   >
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/docs">Docs</NavLink>
-    <NavLink to="/repl">REPL</NavLink>
+    <Logo to="/" as={Link} css={hoverStyles} borderRadius="xs" padding="3" />
+    {isArray(props.links) ? props.links.map(mapLink) : null}
   </Box>
+)
+
+export const NavLink = (props) => (
+  <Text
+    as={Link}
+    css={hoverStyles}
+    textStyle="caps"
+    lineHeight="single"
+    fontSmoothing="antialiased"
+    borderRadius="xs"
+    marginRight="3"
+    paddingX="12"
+    paddingY="3"
+    {...props}
+  />
 )
