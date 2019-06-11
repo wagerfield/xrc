ed72983cd2e65fc69eee6f7a0ef1d9d0bf56d6af
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-11 10:31:03 +0100

    feat: add Wrapper component

---
+++ packages/xrc-app/src/components/wrapper.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Box } from "xrc"
+
+export const Wrapper = (props) => (
+  <Box
+    size="1"
+    padding="4"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    {...props}
+  />
+)
