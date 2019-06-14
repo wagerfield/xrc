b555a2a66de9661cea07e851d0cc75ab53953c74
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-14 08:03:47 +0100

    feat: add style and props interfaces

--- packages/xrc/src/components/ui.tsx
+++ packages/xrc/src/components/ui.tsx
@@ -1,7 +1,11 @@
 import { uiSet, UISetProps } from "../renderers/ui"
 import { component } from "../core/factory"
 
-export const withUIStyles = component<UISetProps>({
+export type UIStyleProps = UISetProps
+
+export type UIProps = UIStyleProps
+
+export const withUIStyles = component<UIProps>({
   name: "UI",
   renderers: [uiSet]
 })
