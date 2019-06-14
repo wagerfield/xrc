6f484518db71913bf3da417654580159f9863f7a
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-14 08:05:14 +0100

    feat: add variant prop filter and compose with ui

--- packages/xrc/src/components/button.tsx
+++ packages/xrc/src/components/button.tsx
@@ -1,27 +1,27 @@
 import { buttonStyle } from "onno"
 import { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react"
 import { PolymorphProps, VariantProps } from "../types/component"
-import { component, polymorph, variant } from "../core/factory"
-import { UISetProps } from "../renderers/ui"
+import { component, polymorph, variant, VARIANT_KEYS } from "../core/factory"
+import { withUIStyles } from "./ui"
 import { IconVariant } from "./icon"
 
 export type ButtonAttributes =
   | AnchorHTMLAttributes<HTMLAnchorElement>
   | ButtonHTMLAttributes<HTMLButtonElement>
 
-export interface ButtonSizeMap {
-  sm: string
-  md: string
-  lg: string
-}
+type ButtonIndexVariant = 0 | "0" | 1 | "1" | 2 | "2"
 
-export type ButtonSize = 0 | "sm" | 1 | "md" | 2 | "lg"
+export type ButtonSize = "sm" | "md" | "lg" | ButtonIndexVariant
 
-export type ButtonVariant = 0 | "primary" | 1 | "secondary" | 2 | "alternative"
+export type ButtonVariant =
+  | "primary"
+  | "secondary"
+  | "alternative"
+  | ButtonIndexVariant
 
 export type ButtonVariantProps = VariantProps<ButtonVariant>
 
-export type ButtonStyleProps = ButtonVariantProps & UISetProps
+export type ButtonStyleProps = ButtonVariantProps
 
 export interface ButtonProps extends ButtonStyleProps, PolymorphProps {
   icon?: IconVariant
@@ -31,20 +31,6 @@ export interface ButtonProps extends ButtonStyleProps, PolymorphProps {
   text?: string
 }
 
-// const DEFAULT_SIZE: ButtonSize = "lg"
-
-// const FONT_SIZE: ButtonSizeMap = {
-//   sm: "14px",
-//   md: "14px",
-//   lg: "16px"
-// }
-
-// const PADDING: ButtonSizeMap = {
-//   sm: "8px 16px",
-//   md: "12px 16px",
-//   lg: "16px 32px"
-// }
-
 export const buttonVariant = variant<ButtonVariantProps>({
   themeKey: "button",
   renderer: buttonStyle
@@ -53,13 +39,12 @@ export const buttonVariant = variant<ButtonVariantProps>({
 export const withButtonStyles = component<ButtonProps>({
   name: "Button",
   renderers: [buttonVariant],
-  styles: (props) => ({
+  pickKeys: VARIANT_KEYS.concat("theme"),
+  styles: {
     variant: "primary"
-    // padding: PADDING[props.size || DEFAULT_SIZE],
-    // fontSize: FONT_SIZE[props.size || DEFAULT_SIZE]
-  })
+  }
 })
 
-// withUIStyles
+const BaseButton = withButtonStyles(polymorph<ButtonAttributes>("button"))
 
-export const Button = withButtonStyles(polymorph<ButtonAttributes>("button"))
+export const Button = withUIStyles(BaseButton)
